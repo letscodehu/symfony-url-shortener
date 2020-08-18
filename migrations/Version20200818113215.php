299dbe20c29730f3748d684f3bf9875ba1c55223
tacsiazuma <fejlesztes@letscode.hu>
2020-08-18 13:38:40 +0200

    URL-shortener-8

---
+++ migrations/Version20200818113215.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200818113215 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE short_url (target VARCHAR(255) NOT NULL COLLATE BINARY, source VARCHAR(16) NOT NULL COLLATE BINARY)');
+        $this->addSql('CREATE INDEX IDX_833605315F8A7F73 ON short_url (source)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE short_url');
+    }
+}
