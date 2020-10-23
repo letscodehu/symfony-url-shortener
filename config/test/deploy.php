19fe70909e368e5fb78a5d7f5266368409e32568
tacsiazuma <fejlesztes@letscode.hu>
2020-10-23 14:18:14 +0200

    URL-shortener-17

---
+++ config/test/deploy.php
@@ -0,0 +1,38 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+
+return new class extends DefaultDeployer
+{
+    public function configure()
+    {
+        return $this->getConfigBuilder()
+            // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
+            ->server('admin@192.168.0.40')
+            // the absolute path of the remote server directory where the project is deployed
+            ->deployDir('/var/www/html')
+            ->remoteComposerBinaryPath("/usr/bin/composer")
+            ->composerInstallFlags("--dev")
+            // the URL of the Git repository where the project code is hosted
+            ->repositoryUrl('git@github.com:letscodehu/symfony-url-shortener.git')
+            // the repository branch to deploy
+            ->repositoryBranch('master');
+    }
+
+    public function beforePreparing() {
+        $this->runRemote('cp {{ deploy_dir }}/repo/.env.test {{ project_dir }}/.env');
+    }
+
+    // run some local or remote commands before the deployment is started
+    public function beforeStartingDeploy()
+    {
+        // $this->runLocal('./vendor/bin/simple-phpunit');
+    }
+
+    // run some local or remote commands after the deployment is finished
+    public function beforeFinishingDeploy()
+    {
+        // $this->runRemote('{{ console_bin }} app:my-task-name');
+        // $this->runLocal('say "The deployment has finished."');
+    }
+};
